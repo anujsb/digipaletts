21dc0af695ac63e97526795f97c8fff741d00018
anuj <anujsbhuyar@gmail.com>
2024-01-27 18:35:40 +0530

    hero final final

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,20 +5,16 @@ import { RxDotFilled } from 'react-icons/rx';
 function Hero() {
   const slides = [
     {
-      url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
+      url: 'https://i.postimg.cc/P5VHyGb2/IMG-20240114-WA0009.jpg',
+      text: 'We are dedicated to your growth and Progress.',
     },
     {
-      url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+      url: 'https://i.postimg.cc/4Ncg9Jcm/Whats-App-Image-2024-01-17-at-16-34-05-656c8949.jpg',
+      text: 'More than digital marketers , we are growth hackers.',
     },
     {
-      url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
-    },
-
-    {
-      url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
-    },
-    {
-      url: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
+      url: 'https://i.postimg.cc/xTB1wxvb/Whats-App-Image-2024-01-17-at-16-34-04-aeea2161.jpg',
+      text: 'Hand over your marketing needs to us, and watch your business grow',
     },
   ];
 
@@ -41,11 +37,15 @@ function Hero() {
   };
 
   return (
-    <div className='max-w-[1400px] h-[780px] w-full m-auto  relative group'>
+    <div className='max-w-[1400px] h-screen w-full m-auto  relative group'>
       <div
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-        className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
-      ></div>
+        className='w-full h-full rounded-2xl bg-center bg-cover duration-1000 relative'
+      >
+        <div className="absolute inset-0 flex items-center justify-center text-black text-4xl font-bold m-20 text-center">
+          {slides[currentIndex].text}
+        </div>
+      </div>
       {/* Left Arrow */}
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
         <BsChevronCompactLeft onClick={prevSlide} size={30} />
@@ -59,7 +59,7 @@ function Hero() {
           <div
             key={slideIndex}
             onClick={() => goToSlide(slideIndex)}
-            className='text-2xl cursor-pointer'
+            className={`text-2xl cursor-pointer ${currentIndex === slideIndex ? 'text-black' : 'text-gray-500'}`}
           >
             <RxDotFilled />
           </div>
