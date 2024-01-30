4a4ca3bd97740e46093a9c7ee65bfb69673cf4c0
anuj <anujsbhuyar@gmail.com>
2024-01-30 14:59:40 +0530

    margin hero mobile view

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -42,10 +42,12 @@ function Hero() {
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
         className='w-full h-full rounded-2xl bg-center bg-cover duration-1000 relative'
       >
-        <div className="absolute inset-0 flex items-center justify-center text-black text-4xl font-bold m-32 text-center duration-1000">
+        <div className="absolute inset-0 flex items-center justify-center text-black text-4xl  font-bold md:m-32 lg:m-32 sm:m-2 text-center duration-1000">
           {slides[currentIndex].text}
         </div>
+        
       </div>
+      
       {/* Left Arrow */}
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
         <BsChevronCompactLeft onClick={prevSlide} size={30} />
